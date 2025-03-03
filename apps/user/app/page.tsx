2f4c0bb78d351a8fc4a00b89bac374b0bbbdd37f
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-03-03 10:57:17 +0530

    adding recoil as store to statemanagement

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,7 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { PrismaClient } from "../../../packages/db";
+
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
