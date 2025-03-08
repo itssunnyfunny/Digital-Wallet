a92109510ca7aee98300242645bb5b0fdde2983a
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-03-08 13:45:51 +0530

    lot of css and name typo fixed

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -4,7 +4,7 @@ import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
-import { TextInput } from "@repo/ui/textinput";
+import { TextInput } from "@repo/ui/textInput";
 
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
